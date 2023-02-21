648f6b97d4bb9ab29a2bc9848507039eb84697c3
TrinhCamMinh <trinhcamminh25112002@gmail.com>
2023-02-21 14:34:21 +0700

    update foods schema

--- src/resources/controllers/customerController.js
+++ src/resources/controllers/customerController.js
@@ -1,15 +1,20 @@
 const { foodModel } = require('../models/');
 
 const homePage = async (req, res) => {
-    const data = await foodModel.find({}).lean();
-    res.render('./Customer/home', { data });
+    try {
+        const data = await foodModel.find({}).populate('ingredients').lean();
+        // res.render('./Customer/home', { data });
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 };
 
 const filter = async (req, res) => {
     try {
         const type = req.query.type;
-        const data = await foodModel.find({ type }).lean();
-        res.status(200).json(data);
+        const filter = await foodModel.find({ type }).lean();
+        res.render('./Customer/home', { filter });
     } catch (error) {
         res.status(500).json(error.message);
     }
